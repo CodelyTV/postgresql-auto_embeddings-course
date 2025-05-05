3dcc377c94aa1dc5406ebfeb754d461318088f6c
Rafa Gómez <rgomezcasas@gmail.com>
2025-05-05 18:20:40 +0200

    feat(02-1): add example

--- 02-generate_embedding/1-generate_with_pg_sql_http/scripts/3-generate_embedding_fn.sql
+++ 02-generate_embedding/1-generate_with_pg_sql_http/scripts/3-generate_embedding_fn.sql
@@ -6,34 +6,22 @@ AS
 $$
 DECLARE
 	text_content TEXT;
-	request_id BIGINT;
 	response_body jsonb;
 	embedding_array DOUBLE PRECISION[];
-	api_url TEXT := 'http://1-generate_on_trigger-ollama-1:11434/api/embeddings';
+	api_url TEXT := 'http://1-generate_with_pg_sql_http-ollama-1:11434/api/embeddings';
 BEGIN
 	text_content := new.name || ' ' || new.summary;
 
-
-	SELECT net.http_post(
-		url := api_url,
-		body := JSONB_BUILD_OBJECT(
-			'model', 'nomic-embed-text',
-			'prompt', text_content
-		),
-		headers := JSONB_BUILD_OBJECT('Content-Type', 'application/json')
-	)
-	INTO request_id;
-
-	RAISE WARNING 'Request ID: %', request_id;
-
-	-- PERFORM pg_sleep(10);
-
 	SELECT content::jsonb
 	INTO response_body
-	FROM net._http_response
-	WHERE id = request_id;
-
-	RAISE WARNING 'Embedding: %', response_body;
+	FROM http_post(
+		api_url,
+		JSONB_BUILD_OBJECT(
+			'model', 'nomic-embed-text',
+			'prompt', text_content
+		)::text,
+		'application/json'
+	 );
 
 	SELECT ARRAY_AGG(e::DOUBLE PRECISION)
 	INTO embedding_array
