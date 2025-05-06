b2b6c3c5f1c3f4f0c195d665f4100801676c717b
Rafa Gómez <rgomezcasas@gmail.com>
2025-05-06 16:45:34 +0200

    feat(03-1): add infra

--- 03-api_call/1-endpoint_by_text/databases/1-enable-extensions.sql
+++ 03-api_call/1-endpoint_by_text/databases/1-enable-extensions.sql
@@ -1,11 +1,4 @@
 CREATE ROLE postgres WITH LOGIN;
 
-create extension if not exists vector;
-
-create extension if not exists pgmq;
-
-create extension if not exists pg_net;
-
-create extension if not exists pg_cron;
-
-create extension if not exists hstore;
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS pg_net;
