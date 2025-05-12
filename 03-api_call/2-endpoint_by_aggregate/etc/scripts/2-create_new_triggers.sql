995c9e86ecf525da690b792b26727eff69a5792f
Rafa Gómez <rgomezcasas@gmail.com>
2025-05-12 17:37:17 +0200

    feat(03-2): improve example

--- 03-api_call/2-endpoint_by_aggregate/etc/scripts/2-create_new_triggers.sql
+++ 03-api_call/2-endpoint_by_aggregate/etc/scripts/2-create_new_triggers.sql
@@ -1,28 +1,15 @@
 CREATE OR REPLACE FUNCTION generate_course_embedding()
-	RETURNS TRIGGER AS $$
+RETURNS TRIGGER AS $$
 BEGIN
-	IF TG_OP = 'INSERT' THEN
-		PERFORM net.http_post(
-			url := 'http://host.docker.internal:3000/api/embeddings',
-			body := JSONB_BUILD_OBJECT(
-				'entity', 'course',
-				'id', NEW.id
-			),
-			headers := JSONB_BUILD_OBJECT('Content-Type', 'application/json')
-		);
-	ELSIF TG_OP = 'UPDATE' THEN
-		IF NEW.name IS DISTINCT FROM OLD.name OR NEW.summary IS DISTINCT FROM OLD.summary THEN
-			PERFORM net.http_post(
-				url := 'http://host.docker.internal:3000/api/embeddings',
-				body := JSONB_BUILD_OBJECT(
-					'entity', 'course',
-					'id', NEW.id
-				),
-				headers := JSONB_BUILD_OBJECT('Content-Type', 'application/json')
-			);
-		END IF;
-	END IF;
-	RETURN NEW;
+  PERFORM net.http_post(
+    url := 'http://host.docker.internal:3000/api/embeddings',
+    body := JSONB_BUILD_OBJECT(
+      'entity', 'course',
+      'id', NEW.id
+    ),
+    headers := JSONB_BUILD_OBJECT('Content-Type', 'application/json')
+  );
+  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
