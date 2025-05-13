621f57b579a3bd4a75250f4d0c0022e872842536
Rafa Gómez <rgomezcasas@gmail.com>
2025-05-13 12:50:43 +0200

    feat(02-2): add example

--- 02-generate_embedding_sync/2-reuse_embedding_generation/scripts/1-add_params_to_fn.sql
+++ 02-generate_embedding_sync/2-reuse_embedding_generation/scripts/1-add_params_to_fn.sql
@@ -1,3 +1,43 @@
+
+CREATE OR REPLACE FUNCTION generate_embedding(
+)
+	RETURNS TRIGGER
+	LANGUAGE plpgsql
+AS
+$$
+DECLARE
+	embedding_input_func_name TEXT = tg_argv[0];
+	text_content TEXT;
+	response_body jsonb;
+	embedding_array DOUBLE PRECISION[];
+	api_url TEXT := 'http://2-reuse_embedding_generation-ollama-1:11434/api/embeddings';
+	embedding_input_query_string TEXT;
+BEGIN
+	embedding_input_query_string := 'SELECT ' || embedding_input_func_name || '($1)';
+
+	EXECUTE embedding_input_query_string INTO text_content USING new;
+
+	SELECT content::jsonb
+	INTO response_body
+	FROM http_post(
+		api_url,
+		JSONB_BUILD_OBJECT(
+			'model', 'nomic-embed-text',
+			'prompt', text_content
+		)::TEXT,
+		'application/json'
+	 );
+
+	SELECT ARRAY_AGG(e::DOUBLE PRECISION)
+	INTO embedding_array
+	FROM JSONB_ARRAY_ELEMENTS_TEXT(response_body -> 'embedding') AS e;
+
+	new.embedding = embedding_array::vector;
+
+	RETURN new;
+END;
+$$;
+
 CREATE OR REPLACE FUNCTION mooc.courses__generate_embedding_input(
 	course mooc.courses
 )
@@ -7,7 +47,7 @@ CREATE OR REPLACE FUNCTION mooc.courses__generate_embedding_input(
 AS
 $$
 BEGIN
-	RETURN '# ' || course.name || E'\n\n' || course.summary; -- La E significa que contiene secuencia de escape
+	RETURN '# ' || course.name || E'\n\n' || course.summary;
 END;
 $$;
 
@@ -22,41 +62,3 @@ CREATE OR REPLACE TRIGGER trg__courses__generate_embedding_before_update
 	ON mooc.courses
 	FOR EACH ROW
 EXECUTE FUNCTION generate_embedding('mooc.courses__generate_embedding_input');
-
-CREATE OR REPLACE FUNCTION generate_embedding(
-)
-	RETURNS TRIGGER
-	LANGUAGE plpgsql
-AS
-$$
-DECLARE
-	embedding_input_func_name TEXT = tg_argv[0];
-	text_content TEXT;
-	response_body jsonb;
-	embedding_array DOUBLE PRECISION[];
-	api_url TEXT := 'http://2-reuse_embedding_generation-ollama-1:11434/api/embeddings';
-	query_string TEXT;
-BEGIN
-	query_string := 'SELECT ' || embedding_input_func_name || '($1)';
-	EXECUTE query_string INTO text_content USING new;
-
-	SELECT content::jsonb
-	INTO response_body
-	FROM http_post(
-		api_url,
-		JSONB_BUILD_OBJECT(
-			'model', 'nomic-embed-text',
-			'prompt', text_content
-		)::TEXT,
-		'application/json'
-		 );
-
-	SELECT ARRAY_AGG(e::DOUBLE PRECISION)
-	INTO embedding_array
-	FROM JSONB_ARRAY_ELEMENTS_TEXT(response_body -> 'embedding') AS e;
-
-	new.embedding = embedding_array::vector;
-
-	RETURN new;
-END;
-$$;
